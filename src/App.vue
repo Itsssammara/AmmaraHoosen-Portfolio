d36946344b3bec306436497cc01622535afa4f65
Itsssammara <hoosenammara@gmail.com>
2024-01-11 09:55:08 +0200

    added new text

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
  <navigation/>
+ 
 </template>
 
 <script>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,9 +1,36 @@
 <template>
     <div class="projects">
-        <h1>Projects</h1>
-
+     <div ><h1>Projects </h1></div>
+       <div class="cards">
+        
+            <div v-for="project in $store.state.projects" :key="project.id" class="project-card">
+                <img :src="project.url" class="project-image" alt="projectcard">
+                <div class="projectinfo">
+                    <h2 class="project-name">{{ project.name }}</h2>
+                    <p class="project-text">{{ project.description }}</p>
+                </div>
+            </div>
+       </div>
     </div>
 </template>
+
+<script>
+export default{
+    // data() {
+    //     return {
+    //         projectData: projectData,
+    //     };
+    // },
+    computed:{
+        fetchProjects(){
+            this.$store.dispatch('fetchProjects')
+        }
+    },
+    mounted(){
+        this.fetchProjects
+    }
+};
+</script>
 <style scoped>
  h1{
   font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
@@ -13,7 +40,7 @@
  text-align: center;
  background: fixed;
  width: auto;
- background-size: cover; /* Adjust as needed */
- background-position: center; /* Adjust as needed */
+ background-size: cover; 
+ background-position: center; 
 }
 </style>
